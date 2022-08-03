3cb909694bd10bfa7b2907243731645623a71cfb
jenngold20 <jennifer.goldfeld@usal.edu.ar>
2022-08-02 22:10:06 -0300

    Ejercicio I

---
+++ clase1/EjercitacionPG.java
@@ -0,0 +1,20 @@
+package clase1;
+
+public class EjercitacionPG {
+    public static void main(String[] args) {
+
+        int numeroEntero = 10;
+        double numeroConComa = 2.4;
+
+        double sumar (num1, num2) {
+            num1 + num2;
+
+        double suma = sumar(numeroEntero, numeroConComa);
+            // No modificar esta linea
+            return suma;
+
+
+        }
+
+    }
+}
---
+++ clase1/animales.java
@@ -0,0 +1,43 @@
+package clase1;
+
+public class animales {
+        public static void main(String[] args) {
+
+            String nombrePerro = "Manchitas" ;
+            int anosPerro = 2;
+            int gramosQueComePerro = 1500 ;
+            String sonidoPerro = "guau guau" ;
+            System.out.println( nombrePerro + " tiene " + anosPerro + " anios.");
+            System.out.println( nombrePerro + " come " + gramosQueComePerro + " gramos, y hace " + sonidoPerro );
+
+            String nombrePez = "Nemo" ;
+            int anosPez = 1;
+            int gramosQueComePez = 140 ;
+            String sonidoPez = "glu glu" ;
+            System.out.println( nombrePez + " tiene " + anosPez + " anio.");
+            System.out.println( nombrePez + " come " + gramosQueComePez + " gramos, y hace " + sonidoPez );
+
+
+            String nombreGato = "Silvestre" ;
+            int anosGato = 3;
+            int gramosQueComeGato = 500 ;
+            String sonidoGato = "miau miau" ;
+            System.out.println( nombreGato + " tiene " + anosGato + " anios.");
+            System.out.println( nombreGato + " come " + gramosQueComeGato + " gramos, y hace " + sonidoGato );
+
+            String nombreTortuga = "Manuelita" ;
+            int anosTortuga = 12;
+            int gramosQueComeTortuga = 300 ;
+            String viveEn = "pehuajo" ;
+            System.out.println( nombreTortuga + " tiene " + anosTortuga + " anios.");
+            System.out.println( nombreTortuga + " come " + gramosQueComeTortuga + " gramos, y vive en " + viveEn );
+
+            String nombrePajaro = "Gardel" ;
+            int anosPajaro = 1;
+            int gramosQueComePajaro = 350 ;
+            String sonidoPajaro = "pio pio" ;
+            System.out.println( nombrePajaro + " tiene " + anosPajaro + " anio.");
+            System.out.println( nombrePajaro + " come " + gramosQueComePajaro + " gramos, y hace " + sonidoPajaro );
+
+        }
+    }
