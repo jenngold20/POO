5e4919c1c4698cd50ff025af2cf04452155c4e7e
jenngold20 <jennifer.goldfeld@usal.edu.ar>
2023-10-16 17:43:03 -0300

    Clase 1

---
+++ clase1/Braulio.java
@@ -0,0 +1,16 @@
+package clase1;
+
+public class Braulio {
+
+    public static void main(String[] args) {  // psvm o main
+
+        String nombre = "Braulio";
+        String color = "blanco";
+        double produccionSemanal = 1250.40;
+        String raza = "criollo";
+        String tipoAnimal = "caballo";
+
+        System.out.println("El " + tipoAnimal + " " + nombre + " es de raza " + raza + ", de color " + color + " y produce semanalmente $" + produccionSemanal);
+
+    }
+}
